fff05f2ecf55edf118fcd72040ff86473e8ba074
Gregory Loomis <cmusicfan2000@gmail.com>
2016-02-06 11:02:47 -0600

    Tab Selection Functionality Added

--- Plate/Plate/MainPage.xaml.cs
+++ Plate/Plate/MainPage.xaml.cs
@@ -26,5 +26,39 @@ namespace Plate
         {
             this.InitializeComponent();
         }
+
+        // ----------------- //
+        // Tab Header Tapped //
+        // ----------------- //
+        private void TabHeader_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            // Get the quadrant of the selected tab
+            string quadrant = ((Controls.TabHeader)sender).Quadrant;
+
+            // Deselect all other tab headers
+            if (quadrant != ShortTermGoals_Tab.Quadrant)
+            {
+                ShortTermGoals_Tab.IsSelected = false;
+            }
+            if (quadrant != LongTermGoals_Tab.Quadrant)
+            {
+                LongTermGoals_Tab.IsSelected = false;
+            }
+            if (quadrant != Distractions_Tab.Quadrant)
+            {
+                Distractions_Tab.IsSelected = false;
+            }
+            if (quadrant != TimeWasters_Tab.Quadrant)
+            {
+                TimeWasters_Tab.IsSelected = false;
+            }
+            if (quadrant != Summary_Tab.Quadrant)
+            {
+                Summary_Tab.IsSelected = false;
+            }
+
+            // Select the current tab header
+            ((Controls.TabHeader)sender).IsSelected = true;
+        }
     }
 }
