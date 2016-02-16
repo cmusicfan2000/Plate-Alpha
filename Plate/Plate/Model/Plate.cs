20d2a5b687f17e04ccd13d53657d4c077277b2f4
Gregory Loomis <cmusicfan2000@gmail.com>
2016-02-15 20:35:22 -0600

    Created the Plate model

---
+++ Plate/Plate/Model/Plate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace Plate.Model
+{
+    class Plate
+    {
+        /// <summary>
+        /// Gets or sets the ID for this plate
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the plate
+        /// </summary>
+        public string name { get; set; }
+    }
+}
