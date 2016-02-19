6af715b1c353dfd8224434ce874bb4c05dbca349
Gregory Loomis <cmusicfan2000@gmail.com>
2016-02-19 10:27:26 -0600

    Created Data Namespace

---
+++ Plate/Plate/ViewModel/PlateItemsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plate.ViewModel
+{
+    class PlateItemsViewModel : GenericViewModel
+    {
+        // ----- //
+        // Tasks //
+        // ----- //
+        private ObservableCollection<TaskViewModel> _tasks;
+        public ObservableCollection<TaskViewModel> tasks
+        {
+            get
+            {
+                return _tasks;
+            }
+            set
+            {
+                _tasks = value;
+                RaisePropertyChanged("tasks");
+            }
+        }
+    }
+}
